9b5a0e62907bce1ebcbc8b082ef31d8c16debf06
francisco04tauan-cloud <francisco04tauan@gmail.com>
2025-10-20 14:05:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,76 +2,80 @@ import mysql.connector
 
 
 def conectar():
-  conexao = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="",
-    database="biblioteca"
-  )
-  return conexao
+    conexao = mysql.connector.connect(
+        host="localhost", user="root", password="", database="biblioteca"
+    )
+    return conexao
 
 
 def adicionar_livro(titulo, autor, ano_publicacao):
-  conexao = conectar()
-  cursor = conexao.cursor()
+    conexao = conectar()
+    cursor = conexao.cursor()
 
-  sql = "INSERT INTO livros(titulo, autor, ano_publicacao) VALUES (%s, %s, %s)"
-  
-  valores = (titulo, autor, ano_publicacao)
-  cursor.execute(sql,valores)
-  conexao.commit()
-  conexao.close()
-  print("Livro Adicionado com sucesso!!!!")
-  
+    sql = "INSERT INTO livros(titulo, autor, ano_publicacao) VALUES (%s, %s, %s)"
+
+    valores = (titulo, autor, ano_publicacao)
+    cursor.execute(sql, valores)
+    conexao.commit()
+    conexao.close()
+    print("Livro Adicionado com sucesso!!!!")
+
+
 def listar_livros():
-  conexao = conectar()
-  cursor = conexao.cursor()
-  
-  cursor.execute("SELECT * FROM livros")
-  livros = cursor.fetchall #pega todos os resultados 
-  conexao.close()
-  
-  if livros:
-    print("lista de livros: ")
-    for livro in livros:
-      print(f"ID: {livro[0]} | Título: {livro[1]} | Autor: {livro[2]} | Ano: {livro[3]}")
-  else:
-    print("Nenhum livro encontrado.")
+    conexao = conectar()
+    cursor = conexao.cursor()
 
-#Atualizar os livros
+    cursor.execute("SELECT * FROM livros")
+    livros = cursor.fetchall  # pega todos os resultados
+    conexao.close()
 
-def atualizar_livros(id_livro, novo_titulo, novo_autor, novo_ano ):
-  conexao = conectar()
-  cursor = conexao.cursor()
-  
-  sql = """
+    if livros:
+        print("lista de livros: ")
+        for livro in livros:
+            print(
+                f"ID: {livro[0]} | Título: {livro[1]} | Autor: {livro[2]} | Ano: {livro[3]}"
+            )
+    else:
+        print("Nenhum livro encontrado.")
+
+
+# Atualizar os livros
+
+
+def atualizar_livros(id_livro, novo_titulo, novo_autor, novo_ano):
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    sql = """
     UPDATE livros
     SET titulo = %s, autor = %s, ano_pubicacao = %s
     WHERE id %s
   """
-  valores = (novo_titulo, novo_autor, novo_ano, id_livro)
-  
-  cursor.execute(sql, valores)
-  conexao.commit()
-  conexao.close()
-  print("Livro atualizado com sucesso!!!")
-  
-  ##DELETE - EXCLUIR UM LIVRO
-  
-  def excluir_livro(id_livro):
+    valores = (novo_titulo, novo_autor, novo_ano, id_livro)
+
+    cursor.execute(sql, valores)
+    conexao.commit()
+    conexao.close()
+    print("Livro atualizado com sucesso!!!")
+
+
+##DELETE - EXCLUIR UM LIVRO
+
+
+def excluir_livro(id_livro):
     conexao = conectar()
     cursor = conexao.cursor()
 
     cursor.execute("DELETE FROM livros  WHERE id = %s", (id_livro,))
     conexao.commit()
     conexao.close()
-    
+
     print("Livro excluido com sucesso")
 
-    
-    ##MENU 
-    def menu():
-      while true:
+
+##MENU
+def menu():
+    while true:
         print("=== SISTEMA DE BIBLIOTECA ===")
         print("1 - Adicionar")
         print("2 - Listar Livros")
@@ -80,12 +84,10 @@ def atualizar_livros(id_livro, novo_titulo, novo_autor, novo_ano ):
         print("0 - Sair")
 
         opcao = input("Escolha uma opção")
-        
-        #Cadastrar livro
+
+        # Cadastrar livro
         if opcao == "1":
-          titulo = input("Título: ")
-          autor = input("Autor: ")
-          ano = input("Ano de publicação: ")
-          adicionar_livro(titulo,autor,ano)
-          
-    
+            titulo = input("Título: ")
+            autor = input("Autor: ")
+            ano = input("Ano de publicação: ")
+            adicionar_livro(titulo, autor, ano)
